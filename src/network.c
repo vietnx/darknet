4ada5562b67f7ee2aeabc734811b650582f8e92d
AlexeyAB <alexeyab84@gmail.com>
2018-02-21 15:35:09 +0300

    Optimized resizing of region_layer for random=1
    (cherry picked from commit e4ab47dfcedb4c87e5eddf484caa4ac0c020fc9b)

--- src/network.c
+++ src/network.c
@@ -428,6 +428,7 @@ int resize_network(network *net, int w, int h)
         net->input_gpu = cuda_make_array(net->input, net->inputs*net->batch);
         net->truth_gpu = cuda_make_array(net->truth, net->truths*net->batch);
         if(workspace_size){
+		    printf(" try to allocate workspace, ");
             net->workspace = cuda_make_array(0, (workspace_size-1)/sizeof(float)+1);
         }
 		printf(" CUDA allocate done! \n");
