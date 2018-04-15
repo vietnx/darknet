5a18c257d7eb938e2d056a1fddece16e6a44f0cc
AlexeyAB <alexeyab84@gmail.com>
2018-04-15 16:57:05 +0300

    Disable burn_in for FP16 Tensor Cores
    (cherry picked from commit 6aa140b3c6e9917936f73bd538fce4fe20954233)

--- src/parser.c
+++ src/parser.c
@@ -684,6 +684,9 @@ void parse_net_options(list *options, network *net)
     char *policy_s = option_find_str(options, "policy", "constant");
     net->policy = get_policy(policy_s);
     net->burn_in = option_find_int_quiet(options, "burn_in", 0);
+#ifdef CUDNN_HALF
+	net->burn_in = 0;
+#endif
     net->power = option_find_float_quiet(options, "power", 4);
     if(net->policy == STEP){
         net->step = option_find_int(options, "step", 1);
