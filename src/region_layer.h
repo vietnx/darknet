AveesLab/r-tod

Parameter (max) in the cfg-file has an effect on the Yolo-training

src/region_layer.h
@@ -6,7 +6,7 @@
 
 typedef layer region_layer;
 
-region_layer make_region_layer(int batch, int h, int w, int n, int classes, int coords);
+region_layer make_region_layer(int batch, int h, int w, int n, int classes, int coords, int max_boxes);
 void forward_region_layer(const region_layer l, network_state state);
 void backward_region_layer(const region_layer l, network_state state);
 void get_region_boxes(layer l, int w, int h, float thresh, float **probs, box *boxes, int only_objectness, int *map);

vietnx/darknet

(cherry picked from commit a2071e158509cc0120a039638e33af86858eefe5)

src/region_layer.h
@@ -5,7 +5,7 @@
 #include "layer.h"
 #include "network.h"
 
-layer make_region_layer(int batch, int w, int h, int n, int classes, int coords);
+layer make_region_layer(int batch, int w, int h, int n, int classes, int coords, int max_boxes);
 void forward_region_layer(const layer l, network net);
 void backward_region_layer(const layer l, network net);
 void resize_region_layer(layer *l, int w, int h);